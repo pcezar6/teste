a9d5a5a3e882208c113a54463c44d059930cf227
filipedower <31519661+filipedower@users.noreply.github.com>
2021-06-16 18:31:14 -0400

    filipe

---
+++ src/static/places.js
@@ -0,0 +1,18 @@
+const places = [
+  {
+    title: 'Green',
+    description:
+      "Apresentação de Ilhas Maldivas.",
+    imageUrl: process.env.PUBLIC_URL + '/assets/island1.jpg',
+    time: 1500,
+  },
+  {
+    title: 'Bora Bora',
+    description:
+      'BORA BORA.',
+    imageUrl: process.env.PUBLIC_URL + '/assets/island2.jpg',
+    time: 1500,
+  },
+];
+
+export default places;
